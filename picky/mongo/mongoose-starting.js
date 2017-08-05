8c550994d603c2dcec6e346b0c976868d95fef61
Claudio Lener <claudiolener98@gmail.com>
2017-08-04 21:07:41 -0400

    added some more basic commands

--- picky/mongo/mongoose-starting.js
+++ picky/mongo/mongoose-starting.js
@@ -1,11 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost:27017/picky-db');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
 
 const userSchema = mongoose.Schema({
     username: String,
@@ -15,7 +12,17 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // an example
-const Kyle = new User({username: "Kyle", password: "secret"});
+db.once('open', function() {
+  const Kyle = new User({
+    username: "Kyle",
+    password: "secret"
+  });
+
+  Kyle.save(function(err, Kyle) {
+    if (err) return console.error(err);
+  });
+  
+});
 
 
 
