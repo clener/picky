061cae85432d834e1b9fa7c155fc5a971ca9165d
Claudio Lener <claudiolener98@gmail.com>
2017-08-07 21:10:18 -0400

    started basic express app

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // returns all the arguments passed to the request
+  console.log(req.query);
+  res.render('index', {
+    title: 'damn',
+    name: 'Claus', 
+  });
 });
 
 module.exports = router;
